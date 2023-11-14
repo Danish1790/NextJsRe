efe9b28763b2a9da02c4c25558cfb121deb45b60
danish1790 <danish.asif.7190@gmail.com>
2023-11-15 02:08:02 +0500

    controlled

--- nextjsre/app/form/page.js
+++ nextjsre/app/form/page.js
@@ -1,26 +1,47 @@
 "use client"
 
-import React,{useRef} from 'react'
+import React, { useRef, useState } from 'react'
 
 const page = () => {
 
-    let inputRef = useRef(null)
+  // using uncontrolled
+  let inputRef = useRef(null)
 
-    const submitForm = (e) =>{
-        console.log("submit form")
-        e.preventDefault();
-        console.warn("input ref value",inputRef.current.value)
-    }
+  const submitForm = (e) => {
+    console.log("submit form")
+    e.preventDefault();
+    console.warn("input ref value", inputRef.current.value)
+  }
+
+  // controlled
+  const [formData, setFormData] = useState({})
+
+  const getData = (value, name) => {
+    const newData = {[name]:value}
+    setFormData({...formData,...newData})
+  }
+  // console.log("form Data",formData)
+
   return (
     <div>
-        <form onSubmit={submitForm}>
-            <input ref={inputRef} type='text' />
-            <br/>
-            <br/>
-            <input type='text' />
-            <br/>
-            <button>Submit</button>
-        </form>
+      <form onSubmit={submitForm}>
+        <input ref={inputRef} type='text' />
+        <br />
+        <br />
+        <input type='text' />
+        <br />
+        <button>Submit</button>
+      </form>
+
+      <br />
+      <br />
+      <br />
+      <div className='form'>
+        <input type='text' name="name" placeholder='enter name' onChange={(e) => { getData(e.target.value, e.target.name) }} />
+        <input type='number' name="age" placeholder='enter age' onChange={(e) => { getData(e.target.value, e.target.name) }} />
+        <input type='date' name="date" placeholder='enter date' onChange={(e) => { getData(e.target.value, e.target.name) }} />
+        <button onClick={()=>{console.log(formData)}}>Submit</button>
+      </div>
     </div>
   )
 }
